f90b1c86971ee4c385bf666b18036934f4d72d51
Alfred <124003668+chkp-altrevin@users.noreply.github.com>
2025-05-13 07:45:50 -0500

    Update deploy_rancher2.sh

--- scripts/deploy_rancher2.sh
+++ scripts/deploy_rancher2.sh
@@ -13,10 +13,9 @@ id=$(docker run -d \
 if [ ! -e "$HOME/rancher-data/k3s" ]; then
   # Give sometime to bootstrap
   sleep 90
-
   # For convenience keep default password to a file using handy docker filter script and values
-  # docker logs ${id}  2>&1 | grep "Bootstrap Password:" > $HOME/initial-passwd
-  $PROJECT_PATH/scripts/filter_docker.sh logs rancher-ui "Bootstrap Password:" > $HOME/initial-passwd
+  docker logs ${id}  2>&1 | grep "Bootstrap Password:" > $HOME/initial-passwd
+  # $PROJECT_PATH/scripts/filter_docker.sh logs rancher-ui "Bootstrap Password:" > $HOME/initial-passwd
 else
   sleep 60
 fi
