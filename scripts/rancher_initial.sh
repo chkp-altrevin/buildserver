dc41c018f12dd906e8634975b1096f806976429a
Alfred <124003668+chkp-altrevin@users.noreply.github.com>
2025-05-12 20:16:15 -0500

    Update rancher_initial.sh

--- scripts/rancher_initial.sh
+++ scripts/rancher_initial.sh
@@ -1,3 +1,10 @@
 #docker logs ${id}  2>&1 | grep "Bootstrap Password:" > $HOME/initial-passwd
 echo "$(cat "$HOME/initial-passwd")"
-echo "Copy password and visit https://192.168.56.10:8443"
+echo "================================================================================"
+echo "Vagrant & VirtualBox users copy password, visit https://192.168.56.10:8443"
+echo "================================================================================"
+echo "All others copy password and ip address associated to the host"
+ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d/ -f1 | paste -sd' ' -
+echo "================================================================================"
+echo "Use like: https://ip-address:8443"
+echo "================================================================================"
