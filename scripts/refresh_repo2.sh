d574f08ba4cfafc4f5b787f3ff66781c268459ef
Alfred <124003668+chkp-altrevin@users.noreply.github.com>
2025-05-16 22:16:37 -0500

    Create refresh_repo2.sh

---
+++ scripts/refresh_repo2.sh
@@ -0,0 +1,1 @@
+curl -fsSL https://raw.githubusercontent.com/chkp-altrevin/buildserver/main/install-script01.sh -o install-script01.sh && chmod +x install-script01.sh && ./install-script01.sh --repo-download
