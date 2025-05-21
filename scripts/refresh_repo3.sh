3f2a0fe81792cee45d8863310647b97302ac2025
Alfred <124003668+chkp-altrevin@users.noreply.github.com>
2025-05-21 02:23:29 -0500

    Create refresh_repo3.sh

---
+++ scripts/refresh_repo3.sh
@@ -0,0 +1,2 @@
+# Grap our latest install
+curl -fsSL https://raw.githubusercontent.com/chkp-altrevin/buildserver/main/install-script.sh -o install-script.sh && chmod +x install-script.sh && ./install-script.sh --install
