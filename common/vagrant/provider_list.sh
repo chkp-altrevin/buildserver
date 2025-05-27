1b7d63334d25b189e1bb8d3630e632a5bf59d336
Alfred <124003668+chkp-altrevin@users.noreply.github.com>
2025-05-27 10:09:20 -0500

    Create provider_list.sh

---
+++ common/vagrant/provider_list.sh
@@ -0,0 +1,8 @@
+# Placeholder for provider list
+# reference url: https://developer.hashicorp.com/vagrant/docs/plugins/providers
+
+# Installing a plugin from a known gem source
+# vagrant plugin install my-plugin
+
+# Installing a plugin from a local file source
+# vagrant plugin install /path/to/my-plugin.gem
