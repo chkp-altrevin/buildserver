7500087aabaffa065ef870a5d427b0d1d428078f
Alfred <124003668+chkp-altrevin@users.noreply.github.com>
2025-06-05 18:18:08 -0500

    Update refresh_repo3.sh

--- common/scripts/refresh_repo3.sh
+++ common/scripts/refresh_repo3.sh
@@ -1,2 +1,2 @@
 # Grap our latest install
-curl -fsSL https://raw.githubusercontent.com/chkp-altrevin/buildserver/main/install-script.sh -o install-script.sh && chmod +x install-script.sh && ./install-script.sh --install
+curl -fsSL https://raw.githubusercontent.com/chkp-altrevin/buildserver/main/install-script.sh -o install-script.sh && chmod +x install-script.sh && sudo ./install-script.sh --install
