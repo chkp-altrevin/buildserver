89f86129752d53f58faec80a8f77670a64912afe
Alfred <124003668+chkp-altrevin@users.noreply.github.com>
2025-06-05 19:59:36 -0500

    Update remove_rancher.sh

--- common/scripts/remove_rancher.sh
+++ common/scripts/remove_rancher.sh
@@ -2,4 +2,5 @@
 docker ps -a | grep "rancher/rancher" | awk '{print $1}' | xargs docker rm -f
 # uncomment below to clean up all rancher folder and configs essentially a reset
 # sudo rm -rf /home/vagrant/rancher-data
-rm $HOME/initial-passwd
+# rm $HOME/initial-passwd
+sudo rm -rf $HOME/rancher-data $HOME/initial-passwd
