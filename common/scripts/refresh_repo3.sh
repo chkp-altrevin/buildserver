0bfd790deafe92665fe5f9a6a060824a1c0bb143
Alfred <124003668+chkp-altrevin@users.noreply.github.com>
2025-06-25 18:31:33 -0500

    Update refresh_repo3.sh

--- common/scripts/refresh_repo3.sh
+++ common/scripts/refresh_repo3.sh
@@ -1,5 +1,2 @@
-# Grap the latest
-curl -fsSL https://raw.githubusercontent.com/chkp-altrevin/buildserver/main/install-script.sh -o install-script.sh && chmod +x install-script.sh && sudo ./install-script.sh --repo-download
-
-# start the provisioning
-./provision.sh
+# Grab the latest
+curl -fsSL https://raw.githubusercontent.com/chkp-altrevin/buildserver/main/install-script.sh -o install-script.sh && chmod +x install-script.sh && sudo ./install-script.sh --install
