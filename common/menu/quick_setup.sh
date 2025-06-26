a728f8f3eeb810af70621f1cb2c1264f47853cf0
Alfred <124003668+chkp-altrevin@users.noreply.github.com>
2025-06-25 21:58:27 -0500

    Update quick_setup.sh

--- common/menu/quick_setup.sh
+++ common/menu/quick_setup.sh
@@ -40,7 +40,8 @@ display_menu() {
     echo "==================================================="
     echo "B. Manage Provisioning for: $PROJECT_NAME"
     echo "==================================================="
-    echo "Q. Update  | P. Rollback  | N. Restore  | L. Remove"
+    echo "x. Update  | x. Rollback  | x. Restore  | x. Remove"
+    # echo "Q. Update  | P. Rollback  | N. Restore  | L. Remove" removing for now, works on linux not vagrant-vb
     echo "Current Commit:0000000   Timestamp:12345678"
     echo "8. INSTALL APPLICATIONS"
     echo "==================================================="
